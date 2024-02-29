ad2a4281615373edf0038bbb886cbc998346a3ea
dan3988 <40858622+dan3988@users.noreply.github.com>
2024-02-29 10:38:28 +0000

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MediaElementCarouselBug
+{
+	public partial class MainPage : ContentPage
+	{
+		public static readonly string[] Urls =
+		[
+			"https://www.w3schools.com/tags/mov_bbb.mp4",
+			"https://www.w3schools.com/tags/mov_bbb.mp4",
+			"https://www.w3schools.com/tags/mov_bbb.mp4"
+		];
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
